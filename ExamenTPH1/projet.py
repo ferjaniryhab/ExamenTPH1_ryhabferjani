d06a412093aef1a411d78a29d06eac316a31c13e
ryhaaab ferjani <ferjaniryhab1@gmail.com>
2025-03-10 09:26:44 +0100

    projet.py

--- ExamenTPH1/projet.py
+++ ExamenTPH1/projet.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-def somme(T)
-S=0
-for t in T:
-s+=t
-return S
-=======
 Data=[1,3,5]
-Som=sum(Data)
-print('la somme est :',som)
->>>>>>> projeth1
+if Data:
+print('la somme est :',sum(Data))
+print('le min est :',min(Data))
+print('le max est:',max(Data))
+else:
+print('dossier vide')
